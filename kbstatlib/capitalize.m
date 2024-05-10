e735615247e7ee9a98c93c4be2afdc1178497f1d
Kim Joris Boström <kim.bostroem@predimo.com>
2024-05-10 17:40:26 +0200

    capitalize: add optional 2nd argument to make only 1st letter big (default), or 'all'

--- kbstatlib/capitalize.m
+++ kbstatlib/capitalize.m
@@ -1,7 +1,18 @@
-function str = capitalize(str)
+function str = capitalize(str, option)
+
+if nargin < 2
+    option = 'first';
+end
 
 str = char(str);
-idx = regexp([' ' str],'(?<=\s+)\S','start')-1;
-str(idx) = upper(str(idx));
+switch option
+    case 'all'
+        idx = regexp([' ' str],'(?<=\s+)\S','start')-1;
+    otherwise
+        idx = 1;
+end
+if ~isempty(str)
+    str(idx) = upper(str(idx));
+end
 
 end
