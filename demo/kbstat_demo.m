38b1ab0e8d02f008eaea353469f301209a17de3c
Kim Joris Boström <kim.bostroem@predimo.com>
2024-11-06 10:16:10 +0100

    kbstat_demo: Clean-up and improve demo

--- demo/kbstat_demo.m
+++ demo/kbstat_demo.m
@@ -1,8 +1,9 @@
 %% Demo for using the kbstat library
-% For more information, see the header of kbstat.m. For most options there
-% is a default, so they do not all have to be provided. Here are the
-% available options for chosing the distribution and link function.
-%
+% For more information, see the header of kbstat.m. For most options there is 
+% a default, so they do not all have to be provided. Here are the available options 
+% for chosing the distribution and link function.
+%%
+% 
 %       distribution    Distribution used for the GLM fit.
 %                       OPTIONAL, default = 'Normal'.
 %                       Possible values:
@@ -13,6 +14,8 @@
 %                       'gamma'	            Gamma distribution
 %                       'inverseGaussian'	Inverse Gaussian distribution
 %
+%%
+% 
 %       link            Link function used for the GLM fit.
 %                       OPTIONAL, default depends on chosen distribution.
 %                       Possible values:
@@ -24,46 +27,54 @@
 %                       'comploglog'	g(mu) = log(-log(1-mu))
 %                       'reciprocal'	g(mu) = mu.^(-1)
 %                       Scalar p	    g(mu) = mu.^p           Canonical for Gamma (p = -1) and InverseGaussian (p= -2)
+%
 
 fprintf('Initializing...\n');
 
-% Clear workspace and close open figures
+%% Clear workspace and close open figures
+
 clear
 close all
 
-% Restore default path
+%% Restore default path
+
 restoredefaultpath;
 
-% Add library and subfolders to path
+%% Add library and subfolders to path
+
 addpath(genpath('../../kbstat'));
 
-% Provide options
+%% Provide basic options
+
 options = struct; % Init empty structure
 options.inFile = 'Chocolate.csv'; % Input file in long format as CSV table
 options.outDir = 'Results'; % Output folder relative to current working directory
+options.distribution = 'gamma'; % Distribution family of GLM
+options.link = 'log'; % Link function of GLM
+options.fitMethod = 'MPL'; % Fitting method of GLM
+options.outlierMethod = 'quartiles'; % Remove outliers using this function 
+options.showVarNames = 'Levels'; % Write levels with capitalized first letter
 
-% For the linear model, either provide model variables explicitly, or
-% provide a Wilkinson formula instead.
+%% Alternative 1: Explicitly define variable types
+% Define model variables and their roles explicitly
 
-% Option 1: Explicit variables (comment-out if you want to use the Wilkinson formula below)
 options.y = 'Distance'; % Dependent variable of model
 options.yUnits = 'm'; % Units of dependent variable
 options.x = 'Chocolate, Gender'; % Fixed-effect variables
+options.interact = 'Chocolate, Gender'; % Analyse interaction between these variables
 options.id = 'Subject'; % Random-effect variable
-
-% % Option 2: Wilkinson formula (if uncommented, overwrites the model definition above)
-% options.formula = 'Distance ~ Chocolate*Gender + (1|Subject)';
-
-% The following options are entirely optional.
-% See kbstat header for more information.
-%
-% options.interact = 'Chocolate, Gender'; % Analyse interaction between these variables
-% options.distribution = 'gamma'; % Distribution family of GLM
-% options.link = 'log'; % Link function of GLM
-% options.fitMethod = 'MPL'; % Fitting method of GLM
-% options.outlierMethod = 'quartiles'; % Remove outliers using this function 
-% options.showVarNames = 'Levels'; % Write levels with capitalized first letter
-
-
+%% 
 % Call main script with the given options
+
+kbstat(options);
+
+%% Alternative 2: Provide Wilkinson formula
+% The Wilkinson formula uniquely defines all variables and their role. If it 
+% is provided, then it overrides any explicitly defined variables.
+
+options.formula = 'Distance ~ Chocolate*Gender + (1|Subject)';
+options.yUnits = 'm'; % Units of dependent variable
+%% 
+% Call main script with the given options
+
 kbstat(options);
